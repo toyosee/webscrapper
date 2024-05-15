87fc965e82a33843b2954326ca15c2be05c2f7d4
Elijah Abolaji <tyabolaji@gmail.com>
2024-05-15 12:04:02 +0100

    Html class for Mr Dave

--- pythonOOP.py
+++ pythonOOP.py
@@ -60,28 +60,84 @@
 # Let's create classes that inherits from one another
 # Execute same method for all classes to show polymorphism
 
-class Motoci:
-    def __init__(wannan, iri, abin_koyi):
-        wannan.iri = iri
-        wannan.abin_koyi = abin_koyi
+# class Motoci:
+#     def __init__(wannan, iri, abin_koyi):
+#         wannan.iri = iri
+#         wannan.abin_koyi = abin_koyi
 
-    def motsawa(wannan):
-        return "TAFIYA!!"
+#     def motsawa(wannan):
+#         return "TAFIYA!!"
 
-class Mota(Motoci):
+# class Mota(Motoci):
+#     pass
+
+# class JirginRuwa(Motoci):
+#     def motsawa(wannan):
+#         return "TAFIYA TA RUWA!!!"
+
+# class JirginSama(Motoci):
+#     def motsawa(wannan):
+#         return "TAFIYA TA SAMA!!!"
+
+# mota = Mota("Honda", "Accord")
+# jirgin_ruwa = JirginRuwa("Siailor", "Xs22")
+# jirgin_sama = JirginSama("BOEING", "AirBus222")
+
+# for aiki in (mota, jirgin_ruwa, jirgin_sama):
+#     print(f"{aiki.iri} {aiki.abin_koyi} Na yana {aiki.motsawa()}")
+
+# def myfunc():
+#   x = 300
+#   def myinnerfunc():
+#     print(x)
+#   myinnerfunc()
+
+# myfunc() 
+
+# Python Classes
+
+
+class Vehicle:
+    def __init__(self, brand, name, year, color):
+        self.brand = brand
+        self.name = name
+        self.year = year
+        self.color = color
+
+    def details(self):
+        details = f"{self.brand}, {self.name}, {self.year}, {self.color}"
+        print(self.move())
+        return details
+    
+    def move(self):
+        return "Drive"
+
+
+class Car(Vehicle):
     pass
 
-class JirginRuwa(Motoci):
-    def motsawa(wannan):
-        return "TAFIYA TA RUWA!!!"
+class Bike(Vehicle):
+    def __init__(self, brand, name, year, color):
+        super().__init__(brand, name, year, color)
 
-class JirginSama(Motoci):
-    def motsawa(wannan):
-        return "TAFIYA TA SAMA!!!"
+    def move(self):
+        return "RIDE"
 
-mota = Mota("Honda", "Accord")
-jirgin_ruwa = JirginRuwa("Siailor", "Xs22")
-jirgin_sama = JirginSama("BOEING", "AirBus222")
+    def description(self, engine_size, tank, top_speed):
+        return f"{engine_size}, {tank}, {top_speed}"
 
-for aiki in (mota, jirgin_ruwa, jirgin_sama):
-    print(f"{aiki.iri} {aiki.abin_koyi} Na yana {aiki.motsawa()}")
+
+class Plane:
+    pass
+
+class Ship:
+    pass
+
+car = Car("Honda", "Civic", 2019, "Gold")
+bike = Bike("Kawasaki", "Boxer", 2020, "black")
+# print(bike.description("v8","20L" , "230mph"))
+
+# for movement in (car, bike):
+#     print(movement.move())
+
+print(bike.details())
